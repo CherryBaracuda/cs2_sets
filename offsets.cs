abf3b119ba01a68322b2189e262c9775f0fa8442
CherryBaracuda <157744599+CherryBaracuda@users.noreply.github.com>
2024-05-05 11:44:31 +0200

    Add files via upload

---
+++ offsets.cs
@@ -0,0 +1,45 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2024-05-05 09:39:28.853444800 UTC
+
+namespace CS2Dumper.Offsets {
+    // Module: client.dll
+    public static class ClientDll {
+        public const nint dwCSGOInput = 0x1932B10;
+        public const nint dwEntityList = 0x18C9E78;
+        public const nint dwGameEntitySystem = 0x19E75C0;
+        public const nint dwGameEntitySystem_getHighestEntityIndex = 0x1510;
+        public const nint dwGameRules = 0x1926DD0;
+        public const nint dwGlobalVars = 0x1730F20;
+        public const nint dwGlowManager = 0x19271F0;
+        public const nint dwLocalPlayerController = 0x1919588;
+        public const nint dwLocalPlayerPawn = 0x173D5A8;
+        public const nint dwPlantedC4 = 0x192FFB8;
+        public const nint dwPrediction = 0x173D470;
+        public const nint dwSensitivity = 0x1927B18;
+        public const nint dwSensitivity_sensitivity = 0x40;
+        public const nint dwViewAngles = 0x1937EA0;
+        public const nint dwViewMatrix = 0x192B310;
+        public const nint dwViewRender = 0x192BB10;
+        public const nint dwWeaponC4 = 0x18CD2D0;
+    }
+    // Module: engine2.dll
+    public static class Engine2Dll {
+        public const nint dwBuildNumber = 0x518764;
+        public const nint dwNetworkGameClient = 0x517B08;
+        public const nint dwNetworkGameClient_deltaTick = 0x258;
+        public const nint dwNetworkGameClient_getLocalPlayer = 0xF0;
+        public const nint dwNetworkGameClient_getMaxClients = 0x250;
+        public const nint dwNetworkGameClient_signOnState = 0x240;
+        public const nint dwWindowHeight = 0x5D109C;
+        public const nint dwWindowWidth = 0x5D1098;
+    }
+    // Module: inputsystem.dll
+    public static class InputsystemDll {
+        public const nint dwInputSystem = 0x377C0;
+    }
+    // Module: matchmaking.dll
+    public static class MatchmakingDll {
+        public const nint dwGameTypes = 0x1D2290;
+        public const nint dwGameTypes_mapName = 0x1D23B0;
+    }
+}
